fdf0dcbf3cb7a1dd8688295086f05c589fc175c9
Hoang Van Hai <hoangvanhai24012004@gmail.com>
2026-04-11 11:10:55 +0700

    11/4_fix editor

--- apps/web/src/components/modal.tsx
+++ apps/web/src/components/modal.tsx
@@ -6,12 +6,14 @@ import { useModal } from "~/providers/modal";
 
 interface Props {
   children: React.ReactNode;
-  modalSize?: "sm" | "md" | "lg" | "hh";
+  modalSize?: "sm" | "md" | "lg" | "hh" | "xl";
   positionFromTop?: "sm" | "md" | "lg";
   isVisible?: boolean;
   closeOnClickOutside?: boolean;
   centered?: boolean;
   className?: string;
+  hideDefaultStyles?: boolean;
+  onClose?: () => void;
 }
 
 const Modal: React.FC<Props> = ({
@@ -22,6 +24,8 @@ const Modal: React.FC<Props> = ({
   closeOnClickOutside,
   centered = false,
   className,
+  hideDefaultStyles = false,
+  onClose,
 }) => {
   const {
     isOpen,
@@ -38,6 +42,7 @@ const Modal: React.FC<Props> = ({
     md: "max-w-[550px]",
     hh: "max-w-[800px]",
     lg: "max-w-[1100px]",
+    xl: "max-w-[1240px]",
   };
 
   const positionFromTopMap = {
@@ -51,7 +56,9 @@ const Modal: React.FC<Props> = ({
       <Dialog
         as="div"
         className={twMerge("relative z-50", className)}
-        onClose={shouldCloseOnClickOutside ? closeModal : () => null}
+        onClose={
+          shouldCloseOnClickOutside ? (onClose ?? closeModal) : () => null
+        }
       >
         <Transition.Child
           as={Fragment}
@@ -79,7 +86,13 @@ const Modal: React.FC<Props> = ({
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel
-                className={`relative ${centered ? "" : positionFromTopMap[positionFromTop]} w-full transform rounded-lg border border-light-600 bg-white/90 text-left shadow-3xl-light backdrop-blur-[6px] transition-all dark:border-dark-600 dark:bg-dark-100/90 dark:shadow-3xl-dark ${modalSizeMap[modalSize]}`}
+                className={twMerge(
+                  "relative transform transition-all w-full",
+                  !centered && positionFromTopMap[positionFromTop],
+                  modalSizeMap[modalSize],
+                  !hideDefaultStyles &&
+                    "rounded-lg border border-light-600 bg-white/90 text-left shadow-3xl-light backdrop-blur-[6px] dark:border-dark-600 dark:bg-dark-100/90 dark:shadow-3xl-dark",
+                )}
               >
                 {children}
               </Dialog.Panel>
